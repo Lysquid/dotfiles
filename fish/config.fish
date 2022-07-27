ce3c37fefcad7c23b851c9e2b38474e4ed4e1647
Lysquid <romainbenoit21560@gmail.com>
2022-07-27 12:47:00 +0200

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,36 @@
+# Default
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Fish shell for root user when switching with su
+function su
+   command su --shell=/usr/bin/fish $argv
+end
+
+# Remove 'Welcome to fish' message
+set fish_greeting
+
+# Environnement variables
+set -gx BROWSER firefox
+set -gx TERM alacritty
+set -gx LESS '-R --use-color -Dd+r$Du+b'
+set -gx MANPAGER 'less -R --use-color -Dd+r -Du+b'
+set -gx GTK_IM_MODULE fcitx
+set -gx QT_IM_MODULE fcitx
+set -gx XMODIFIERS @im=fcitx
+
+# Path
+set -gx PATH $PATH $HOME/bin
+set -gx PATH $PATH $HOME/.local/bin
+
+# Alias
+alias ip='ip -color=auto'
+alias rickroll='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
+alias tra='gio trash'
+alias trr='gio trash --restore'
+alias trl='gio trash --list'
+
+function brightness
+    command xrandr --output HDMI2 --brightness (math $argv/10)
+end
