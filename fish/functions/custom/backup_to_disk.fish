d793af1567dc583b6ea86c8ba4adf76f2db33e87
Lysquid <romainbenoit21560@gmail.com>
2023-08-14 08:53:54 +0200

    update before holidays

--- fish/functions/custom/backup_to_disk.fish
+++ fish/functions/custom/backup_to_disk.fish
@@ -1,6 +1,6 @@
 function backup_to_disk -d "Backup all useful files to an external disk"
     rsync \
-    -aPht \
+    -aPh \
     --delete \
     --delete-excluded \
     --exclude-from=/home/rom1/.config/rsync/exclude_list.txt \
