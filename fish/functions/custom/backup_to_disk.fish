d21dd0b4d13bb2a8786058e0c513930740617465
Lysquid <romainbenoit21560@gmail.com>
2023-09-30 16:28:48 +0200

    various updates

--- fish/functions/custom/backup_to_disk.fish
+++ fish/functions/custom/backup_to_disk.fish
@@ -6,6 +6,7 @@ function backup_to_disk -d "Backup all useful files to an external disk"
     --exclude-from=/home/rom1/.config/rsync/exclude_list.txt \
     /etc \
     /home \
+    /mnt \
     "/run/media/rom1/Backup Disk/arch" \
     $argv
 end
