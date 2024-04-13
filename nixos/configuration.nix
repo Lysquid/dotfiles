860d7ef20383896d7f8ca74cfb8fc538f44fd518
Lysquid <romainbenoit21560@gmail.com>
2024-04-13 18:10:12 +0200

    moved to flake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,6 +139,7 @@
     wl-gammactl
     xdg-utils
     zip
+    vscodium-fhs
   ];
 
   environment.variables = {
@@ -228,7 +229,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  # system.copySystemConfiguration = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -236,7 +237,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
 
