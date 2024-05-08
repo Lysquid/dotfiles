b4c35b0963271e3685f22417da5408c36d7ae574
Lysquid <romainbenoit21560@gmail.com>
2024-05-08 16:53:15 +0200

    big update

--- fish/conf.d/environement.fish
+++ fish/conf.d/environement.fish
@@ -1,27 +1,26 @@
-# Applications
-set -gx BROWSER firefox
-set -gx EDITOR nvim
-set -gx OPENER mimeopen
-
-# Tweaks
-set -gx MANPAGER 'less -R --use-color -Dd+r -Du+b'
-set -gx LESS '-R --use-color -Dd+r$Du+b'
-set -gx QT_QPA_PLATFORMTHEME qt5ct
-set -gx GTK_THEME Orchis-Dark
-set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
-set -gx GTK2_RC_FILES ~/.config/gtk-2.0/gtkrc
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-set -gx _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on'
-
-# Input
-set -gx GTK_IM_MODULE fcitx
-set -gx QT_IM_MODULE fcitx
-set -gx XMODIFIERS @im=fcitx
-
-# Path
-set -gx PATH $PATH $HOME/.local/bin
-set -gx PATH $PATH $HOME/.config/bin
+# Fish
+set fish_greeting
 set -g fish_function_path $fish_function_path ~/.config/fish/functions/custom
 
 # Plugins
 set sponge_delay 10
+
+if test "$OS" != "nixos"
+
+    # Applications
+    set -gx BROWSER firefox
+    set -gx EDITOR nvim
+    set -gx OPENER mimeopen
+
+    # Tweaks
+    set -gx MANPAGER 'less -R --use-color -Dd+r -Du+b'
+    set -gx LESS '-R --use-color -Dd+r$Du+b'
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+    set -gx GTK2_RC_FILES ~/.config/gtk-2.0/gtkrc
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+    # set -gx _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on'
+
+    # Path
+    set -gx PATH $PATH $HOME/.local/bin
+
+end
--- fish/functions/custom/backup_config.fish
+++ fish/functions/custom/backup_config.fish
@@ -1,19 +1,20 @@
 function backup_config -d "Backup system configuration"
     # installed softwares
-    pacman -Qq > ~/.config/arch/pacman-Qq.txt
-    pacman -Qqe > ~/.config/arch/pacman-Qqe.txt
-    pacman -Qqem > ~/.config/arch/pacman-Qqem.txt
+    pacman -Qq > ~/.config/arch/pacman-all.txt
+    pacman -Qqe > ~/.config/arch/pacman-explicit.txt
+    pacman -Qqem > ~/.config/arch/pacman-aur.txt
+    # pipx
     pipx list --short > ~/.config/arch/pipx.txt
+    # vscode extensions
+    code --list-extensions > ~/.config/arch/code-extensions.txt
 
     # systemd unit files
     systemctl list-unit-files --state=enabled > ~/.config/arch/systemctl-enabled.txt
-    systemctl list-unit-files > ~/.config/arch/systemctl.txt
 
     # edited files not in .config
     rsync --relative \
     /etc/fstab \
     /etc/pacman.conf \
-    /etc/X11/xorg.conf.d/** \
     /etc/default/grub \
     /etc/environment \
     /etc/hostname \
@@ -23,5 +24,7 @@ function backup_config -d "Backup system configuration"
     /etc/libvirt/libvirtd.conf \
     /etc/libvirt/qemu.conf \
     /etc/greetd/config.toml \
+    /etc/udev/rules.d/9*.rules \
+    /etc/ssh/sshd_config \
     .config/
 end
