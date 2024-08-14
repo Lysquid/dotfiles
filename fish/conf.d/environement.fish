87ef2ff78c92bfcc2784fffae04e806827b4fc39
Lysquid <8axop2rj@anonaddy.com>
2024-08-14 22:10:46 +0200

    removed sway

--- fish/conf.d/environement.fish
+++ fish/conf.d/environement.fish
@@ -2,15 +2,13 @@
 set -gx BROWSER firefox
 set -gx EDITOR nvim
 
-# Tweaks
-set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml 
-set -gx GTK2_RC_FILES ~/.config/gtk-2.0/gtkrc
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-
 # Colors
 set -gx MANPAGER 'less -R --use-color -Dd+r -Du+b'
 set -gx MANROFFOPT '-P -c'
 set -gx LESS '-R --use-color -Dd+r$Du+b'
+
+# Tweaks
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
 # Path
 set -gx PATH $PATH $HOME/.local/bin
